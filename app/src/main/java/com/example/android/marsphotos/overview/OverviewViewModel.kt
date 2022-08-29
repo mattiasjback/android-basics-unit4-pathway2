1fea04f2b8c706486bf272865623fac6cb33e39e
Mattias Bäck <mattias.back@bontouch.com>
2022-08-29 15:50:50 +0200

    Downgrade coil and setup live data for photos

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -21,6 +21,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.marsphotos.network.MarsApi
+import com.example.android.marsphotos.network.MarsPhoto
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
@@ -35,6 +36,9 @@ class OverviewViewModel : ViewModel() {
     // The external immutable LiveData for the request status
     val status: LiveData<String> = _status
 
+    private val _photos = MutableLiveData<MarsPhoto>()
+
+    val photos : LiveData<MarsPhoto> = _photos
     /**
      * Call getMarsPhotos() on init so we can display status immediately.
      */
@@ -49,8 +53,8 @@ class OverviewViewModel : ViewModel() {
     private fun getMarsPhotos() {
         viewModelScope.launch {
             try {
-                val result = MarsApi.client.getPhotos()
-                _status.value = "Success: ${result.size} Mars photos retrieved"
+                _photos.value = MarsApi.client.getPhotos().first()
+                _status.value = "Mars image URL ${_photos.value!!.imgSrcUrl}"
             } catch (e: Exception) {
                 _status.value = "Failure: ${e.message}"
             }
